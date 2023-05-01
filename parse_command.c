76f13aa1fc0eb9b549d79b7229c521d365e90129
Brian <gisameht@gmail.com>
2023-05-01 00:10:50 -0400

    pipes

--- parse_command.c
+++ parse_command.c
@@ -1,6 +1,99 @@
 #include "built_in.c"
 
 #define MAX_NUM_ARGUMENTS 50
+#define MAX_HISTORY_LENGTH 10
+#define MAX_COMMAND_LENGTH 100
+///////////////////////
+#define MAX_CMDS 10
+char input[1024];
+char *cmds[MAX_CMDS];
+int num_cmds = 0;
+int pipes[MAX_CMDS - 1][2];
+
+int Pipes(char input[MAX_NUM_ARGUMENTS], char *cmds[MAX_CMDS], int num_cmds, int pipes[MAX_CMDS - 1][2])
+{
+    char *cmd = strtok(input, "|");
+    while (cmd != NULL && num_cmds < MAX_CMDS)
+    {
+        cmds[num_cmds++] = cmd;
+        cmd = strtok(NULL, "|");
+    }
+
+    // Crear los pipes
+    for (int i = 0; i < num_cmds - 1; i++)
+    {
+        if (pipe(pipes[i]) == -1)
+        {
+            perror("pipe");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    // Crear los procesos hijos y ejecutar los comandos
+    for (int i = 0; i < num_cmds; i++)
+    {
+        pid_t pid = fork();
+        if (pid == -1)
+        {
+            perror("fork");
+            exit(EXIT_FAILURE);
+        }
+        else if (pid == 0)
+        {
+            // Proceso hijo
+            if (i > 0)
+            {
+                // Conectar la entrada estándar con el pipe anterior
+                dup2(pipes[i - 1][0], STDIN_FILENO);
+            }
+            if (i < num_cmds - 1)
+            {
+                // Conectar la salida estándar con el siguiente pipe
+                dup2(pipes[i][1], STDOUT_FILENO);
+            }
+            // Cerrar los descriptores de archivo de los pipes
+            for (int j = 0; j < num_cmds - 1; j++)
+            {
+                close(pipes[j][0]);
+                close(pipes[j][1]);
+            }
+            // Ejecutar el comando
+            char cmd_copy[MAX_COMMAND_LENGTH];
+            strncpy(cmd_copy, cmds[i], sizeof(cmd_copy));
+            char *args[MAX_COMMAND_LENGTH];
+            int num_args = 0;
+            char *arg = strtok(cmd_copy, " ");
+            while (arg != NULL && num_args < MAX_COMMAND_LENGTH)
+            {
+                args[num_args++] = arg;
+                arg = strtok(NULL, " ");
+            }
+            args[num_args] = NULL;
+            if (execvp(args[0], args) == -1)
+            {
+                return EXIT_FAILURE;
+            }
+
+            // perror("execvp");
+            // exit(EXIT_FAILURE);
+        }
+    }
+
+    // Cerrar los descriptores de archivo de los pipes en el proceso padre
+    for (int i = 0; i < num_cmds - 1; i++)
+    {
+        close(pipes[i][0]);
+        close(pipes[i][1]);
+    }
+
+    // Esperar a que todos los procesos hijos terminen
+    for (int i = 0; i < num_cmds; i++)
+    {
+        wait(NULL);
+    }
+
+    return EXIT_SUCCESS;
+}
 
 /**
  * @brief El metodo se encarga de procesar cada comando simple o estandarizado.
@@ -10,18 +103,18 @@
  * @param auxiliar1 Esacio donde sera almacenado el comando
  * @param len Indice de auxiliar1
  * @return No devuelve nada
-*/
+ */
 void process_auxiliar(char **arguments, int *last_null, int num_arguments, char **auxiliar1, int *len)
 {
-    int i = *last_null + 1; //creo un indice q empieza en el token siguiente al ultimo null
-    *last_null = num_arguments; //actualizo el ultimo null como el operador actual
-    while (i < num_arguments) //tomo todo lo que hay entre operador y operador
+    int i = *last_null + 1;     // creo un indice q empieza en el token siguiente al ultimo null
+    *last_null = num_arguments; // actualizo el ultimo null como el operador actual
+    while (i < num_arguments)   // tomo todo lo que hay entre operador y operador
     {
-        auxiliar1[*len] = arguments[i]; //lo agrego a auxiliar 
-        i++; //aumento los indices
+        auxiliar1[*len] = arguments[i]; // lo agrego a auxiliar
+        i++;                            // aumento los indices
         (*len)++;
     }
-    auxiliar1[*len] = NULL; //hago null la ultima posicion
+    auxiliar1[*len] = NULL; // hago null la ultima posicion
 }
 
 /**
@@ -30,11 +123,11 @@ void process_auxiliar(char **arguments, int *last_null, int num_arguments, char 
  * @param last_null Puntero al comienzo del comando.
  * @param num_arguments Numero de argumentos tokenizados.
  * @return Devuelve 1 si ocurrio algun error y 0 e.o.c.
-*/
+ */
 int tokenized_util(char **arguments, int *last_null, int num_arguments, int background)
 {
-    char *auxiliar1[MAX_NUM_ARGUMENTS]; //construyo el comando simple que voy a procesar
-    int len = 0; //creo un indice para ir construyendo auxiliar1
+    char *auxiliar1[MAX_NUM_ARGUMENTS]; // construyo el comando simple que voy a procesar
+    int len = 0;                        // creo un indice para ir construyendo auxiliar1
 
     process_auxiliar(arguments, last_null, num_arguments, auxiliar1, &len);
     while ((pid_gen = waitpid(-1, NULL, WNOHANG)) > 0) // terminar el proceso
@@ -54,7 +147,7 @@ int tokenized_util(char **arguments, int *last_null, int num_arguments, int back
     int std_status = std_method(auxiliar1, len, background);
     if (std_status == 1)
         std_status = built_in(auxiliar1, len, background);
-    
+
     return std_status;
 }
 
@@ -62,14 +155,14 @@ int tokenized_util(char **arguments, int *last_null, int num_arguments, int back
  * @brief El metodo se encarga de parsear cada comando y ejecutarlo.
  * @param parsed_arguments Matriz con la entrada tokenizada del usuario.
  * @return No devuelve nada.
-*/
+ */
 void tokenized(char *parsed_arguments, int background)
 {
     char *operators[] = {"|", "||", "&&", NULL};
     char *token;
     int status = 0;
     char *arguments[MAX_NUM_ARGUMENTS]; // Arreglo de punteros a los argumentos del comando
-    int num_arguments = 0; // Inicializar el número de argumentos
+    int num_arguments = 0;              // Inicializar el número de argumentos
     int last_null = -1;
     int or_status = 0;
     int if_status = 0;
@@ -80,6 +173,10 @@ void tokenized(char *parsed_arguments, int background)
     int first_true = 0;
     int piped = 0;
 
+    if (Pipes(parsed_arguments, cmds, num_arguments, pipes) == EXIT_SUCCESS)
+    {
+        return;
+    }
     token = strtok(parsed_arguments, " "); // Obtener el primer token del comando
 
     while (token != NULL && num_arguments < MAX_NUM_ARGUMENTS - 1) // Mientras haya tokens y no se haya alcanzado el número máximo de argumentos
@@ -88,7 +185,7 @@ void tokenized(char *parsed_arguments, int background)
         {
             return;
         }
-        
+
         arguments[num_arguments] = token; // Agregar el token al arreglo de argumentos
 
         if (strcmp(token, "|") == 0)
@@ -113,18 +210,18 @@ void tokenized(char *parsed_arguments, int background)
         {
             if (strcmp(arguments[last_null + 1], "if") == 0)
             {
-                if_status += 1; //esto me da cuantos if tengo actualmente
-                last_null = last_null + 1; //actualizo el ultimo null
+                if_status += 1;            // esto me da cuantos if tengo actualmente
+                last_null = last_null + 1; // actualizo el ultimo null
                 int std_status = tokenized_util(arguments, &last_null, num_arguments, background);
-                else_status = 0; //actualizo los estados de else 
-                else_before_end = 0; //para el nuevo bloque de if
-                if (std_status == 1) //si hubo un error en la condicion
+                else_status = 0;     // actualizo los estados de else
+                else_before_end = 0; // para el nuevo bloque de if
+                if (std_status == 1) // si hubo un error en la condicion
                 {
-                    else_status = 1; //actualizo el else para que se ejecute
+                    else_status = 1; // actualizo el else para que se ejecute
                 }
                 else
                 {
-                    end_status = 1; //actualizo el end para que se ejecute si no hay else
+                    end_status = 1; // actualizo el end para que se ejecute si no hay else
                     first_true = 1;
                 }
             }
@@ -134,29 +231,29 @@ void tokenized(char *parsed_arguments, int background)
         else if (strcmp(token, "else") == 0 && first_true != 2)
         {
             else_before_end = 1;
-            if (else_status == 1) //si hubo error en la condicion
+            if (else_status == 1) // si hubo error en la condicion
             {
-                last_null = num_arguments; //last_null ahora es el else
+                last_null = num_arguments; // last_null ahora es el else
             }
-            else //si no hubo error
+            else // si no hubo error
             {
-                int std_status = tokenized_util(arguments, &last_null, num_arguments, background); //ejecuto el comando entre then y else
-                not_else = 1; //actualizo el estado de no ejecutar lo que hay en el else
+                int std_status = tokenized_util(arguments, &last_null, num_arguments, background); // ejecuto el comando entre then y else
+                not_else = 1;                                                                      // actualizo el estado de no ejecutar lo que hay en el else
                 first_true = 2;
             }
         }
         else if (strcmp(token, "end") == 0)
         {
-            if (first_true == 2 || (not_else == 1 && if_status > 0)) //si no hay que ejecutar el contenido del else y hubo un if al menos
+            if (first_true == 2 || (not_else == 1 && if_status > 0)) // si no hay que ejecutar el contenido del else y hubo un if al menos
             {
-                last_null = num_arguments; //last_null es el end actual
-                if_status -= 1; //quito un if de los que haya sin finalizar
+                last_null = num_arguments; // last_null es el end actual
+                if_status -= 1;            // quito un if de los que haya sin finalizar
             }
-            else if (else_status == 1 && else_before_end == 1) //si hay que ejecutar el contenido del else
+            else if (else_status == 1 && else_before_end == 1) // si hay que ejecutar el contenido del else
             {
-                int std_status = tokenized_util(arguments, &last_null, num_arguments, background); //ejecuto el comando
-                else_status = 0; //actualizo el else como ejecutado
-                if_status -= 1; //quito un if de los que haya sin finalizar
+                int std_status = tokenized_util(arguments, &last_null, num_arguments, background); // ejecuto el comando
+                else_status = 0;                                                                   // actualizo el else como ejecutado
+                if_status -= 1;                                                                    // quito un if de los que haya sin finalizar
             }
             else if (end_status == 1 && else_before_end == 0 && strcmp(arguments[last_null], "end") != 0)
             {
@@ -169,13 +266,13 @@ void tokenized(char *parsed_arguments, int background)
                 if_status -= 1;
             }
 
-            if (if_status == 0) //si no quedan if por finalizar
-                not_else = 0; //actualizo el estado de los else
-            
+            if (if_status == 0) // si no quedan if por finalizar
+                not_else = 0;   // actualizo el estado de los else
+
             else_before_end = 0;
         }
 
-        num_arguments++; // Incrementar el número de argumentos
+        num_arguments++;           // Incrementar el número de argumentos
         token = strtok(NULL, " "); // Obtener el siguiente token del comando
     }
     arguments[num_arguments] = NULL; // Agregar un puntero nulo al final del arreglo de argumentos
@@ -184,6 +281,7 @@ void tokenized(char *parsed_arguments, int background)
         background = 1;
     else
         background = 0;
+    
     if (background)
     {
         arguments[num_arguments - 1] = '\0';
@@ -194,15 +292,15 @@ void tokenized(char *parsed_arguments, int background)
     {
         int fd[2];
 
-        char *args1[MAX_NUM_ARGUMENTS]; //construyo el comando simple que voy a procesar
-        int len1 = 0; //creo un indice para ir construyendo auxiliar1
+        char *args1[MAX_NUM_ARGUMENTS]; // construyo el comando simple que voy a procesar
+        int len1 = 0;                   // creo un indice para ir construyendo auxiliar1
 
         process_auxiliar(arguments, &last_null, piped, args1, &len1);
 
-        char *args2[MAX_NUM_ARGUMENTS]; //construyo el comando simple que voy a procesar
-        int len2 = 0; //creo un indice para ir construyendo auxiliar1
+        char *args2[MAX_NUM_ARGUMENTS]; // construyo el comando simple que voy a procesar
+        int len2 = 0;                   // creo un indice para ir construyendo auxiliar1
         int aux1 = piped;
-        
+
         process_auxiliar(arguments, &aux1, num_arguments, args2, &len2);
 
         if (pipe(fd) == -1)
@@ -213,21 +311,25 @@ void tokenized(char *parsed_arguments, int background)
 
         pid_t pid = fork();
 
-        if (pid == -1) {
+        if (pid == -1)
+        {
             perror("fork");
             exit(EXIT_FAILURE);
         }
 
-        if (pid == 0) {
+        if (pid == 0)
+        {
             // Proceso hijo
-            close(fd[0]); // Cerramos el extremo de lectura del pipe
+            close(fd[0]);               // Cerramos el extremo de lectura del pipe
             dup2(fd[1], STDOUT_FILENO); // Redirigimos la salida extremo de escritura del pipe
-            execvp(args1[0], args1); // Ejecutamos el primer comando
-        } else {
+            execvp(args1[0], args1);    // Ejecutamos el primer comando
+        }
+        else
+        {
             // Proceso padre
-            close(fd[1]); // Cerramos el extremo de escritura del pipe
+            close(fd[1]);              // Cerramos el extremo de escritura del pipe
             dup2(fd[0], STDIN_FILENO); // Redirigimos la entrada estándar al extremo de lectura del pipe
-            execvp(args2[0], args2); // Ejecutamos el segundo comando
+            execvp(args2[0], args2);   // Ejecutamos el segundo comando
         }
     }
 
@@ -247,26 +349,26 @@ void tokenized(char *parsed_arguments, int background)
  * @brief El metodo se encarga de parsear cada comando y ejecutarlo.
  * @param command Entrada del usuario sin procesar
  * @return No devuelve nada.
-*/
+ */
 void parse_command(char *command)
 {
     char *parsed_arguments[MAX_NUM_ARGUMENTS];
     int num_tok = 0;
     int background = 0;
-    
-    char *token = strtok(command, ";"); // Obtener el primer token del comando
 
+    char *token = strtok(command, ";"); // Obtener el primer token del comando
+    /////                                                 ////////////////////////////
     while (token != NULL && num_tok < MAX_NUM_ARGUMENTS - 1) // Mientras haya tokens y no se haya alcanzado el número máximo de argumentos
     {
         parsed_arguments[num_tok] = token; // Agregar el token al arreglo de argumentos
-        num_tok++; // Incrementar el número de argumentos
-        token = strtok(NULL, ";"); // Obtener el siguiente token del comando
+        num_tok++;                         // Incrementar el número de argumentos
+        token = strtok(NULL, ";");         // Obtener el siguiente token del comando
     }
     parsed_arguments[num_tok] = NULL; // Agregar un puntero nulo al final del arreglo de argumentos
 
     int index = 0;
 
-    while(parsed_arguments[index] != NULL && index < MAX_NUM_ARGUMENTS - 1)
+    while (parsed_arguments[index] != NULL && index < MAX_NUM_ARGUMENTS - 1)
     {
         tokenized(parsed_arguments[index], background);
         index++;
